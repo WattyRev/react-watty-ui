6a0b3ae85ee9bb4aea64ad710e21f48efd0eae00
Spencer Watson <wattyrev@gmail.com>
2021-11-30 12:54:29 -0800

    Make modal scrollable

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -46,17 +46,27 @@ const modalAnimation = keyframes`
  * Styling for the modal window itself
  */
 export const StyledModal = styled.div`
-  position: fixed;
-  top: 50px;
+  position: relative;
+  margin-top: 50px;
+  margin-bottom: 50px;
   left: 50%;
   transform: translateX(-50%);
   max-width: 300px;
   background: ${props => props.theme.colors.white};
-  z-index: 100;
   padding: 16px;
   border: 1px solid ${props => props.theme.colors.gray};
   border-radius: 4px;
   animation: ${modalAnimation} 0.8s;
+`;
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: auto;
+  z-index: 100;
 `;
 
 /**
@@ -67,7 +77,9 @@ const Modal = ({ children, isOpen, onBackdropClick }) => (
     {isOpen && (
       <BodyPortal>
         <ModalBackdrop onClick={onBackdropClick} className="testing_backdrop" />
-        <StyledModal className="testing_modal">{children}</StyledModal>
+        <ModalWrapper>
+          <StyledModal className="testing_modal">{children}</StyledModal>
+        </ModalWrapper>
       </BodyPortal>
     )}
   </React.Fragment>
